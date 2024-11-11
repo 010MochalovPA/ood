4763a4e7c17a0c122fab992651e10552f8d649ea
Pavel Mochalov <010mochalovpa@gmail.com>
2024-11-11 22:38:10 +0300

    add UML

--- 01/04/src/models/shapes/strategies/IShapeStrategy.ts
+++ 01/04/src/models/shapes/strategies/IShapeStrategy.ts
@@ -2,7 +2,7 @@ import { ICanvas } from "../../../gfx/ICanvas";
 
 interface IShapeStrategy {
     draw(canvas: ICanvas): void;
-    move(dx: number, dy: number);
+    move(dx: number, dy: number): void;
     toString(): string;
 }
 
